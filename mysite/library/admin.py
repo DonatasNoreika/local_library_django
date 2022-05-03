7b959282797e4668f6447d581c580a1d50544a53
Donatas Noreika <donoras@gmail.com>
2022-05-03 10:07:57 +0300

    3 done

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -6,7 +6,32 @@ from .models import (Book,
                      Genre,
                      Author)
 
-admin.site.register(Book)
-admin.site.register(BookInstance)
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0 # išjungia placeholder'ius
+    readonly_fields = ('uuid',)
+    can_delete = False
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'summary', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'uuid')
+    list_filter = ('status', 'due_back')
+    search_fields = ('uuid', 'book__title')
+    list_editable = ('due_back', 'status')
+
+    fieldsets = (
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    )
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_books')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
 admin.site.register(Genre)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
