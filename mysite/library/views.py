ae5aeb15db744b16a828a8a314cc0347fa0bf8c6
Donatas Noreika <donoras@gmail.com>
2022-05-04 10:35:39 +0300

    5 added books and book pages (class-based)

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book, Author, BookInstance, Genre
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 
 def index(request):
@@ -43,3 +44,14 @@ def authors(request):
 def author(request, author_id):
     single_author = get_object_or_404(Author, pk=author_id)
     return render(request, 'author.html', {'author': single_author})
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
